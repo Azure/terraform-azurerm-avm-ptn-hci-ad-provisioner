ac23b3f482bbb72006b4bf01a5c76ba60bdc4f40
Hangyu Xu <hangxu@microsoft.com>
2024-07-23 14:41:37 +0800

    update

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -52,7 +52,7 @@ module "test" {
   # source             = "Azure/avm-<res/ptn>-<name>/azurerm"
   # ...
   location            = azurerm_resource_group.rg.location
-  name                = "TODO" # TODO update with module.naming.<RESOURCE_TYPE>.name_unique
+  name                = azurerm_resource_group.rg.name # TODO update with module.naming.<RESOURCE_TYPE>.name_unique
   resource_group_name = azurerm_resource_group.rg.name
 
   enable_telemetry = var.enable_telemetry # see variables.tf
